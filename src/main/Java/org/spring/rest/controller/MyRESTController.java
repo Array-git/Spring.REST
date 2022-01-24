f7f7e7bb1290ff9e82eb06caf40c840abde31f40
Array-git <array3@gmail.com>
2022-01-24 17:49:12 +0200

    Don't run. Now no idea how fix.

---
+++ src/main/Java/org/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,24 @@
+package org.spring.rest.controller;
+
+import org.spring.rest.entity.Employee;
+import org.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+}
